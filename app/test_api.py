2eb6d70e79c688ffa83d24677ec48ea328c10ed4
Khánh Hoàng <trongkhanh083@gmail.com>
2025-07-06 00:39:11 +0700

    remove

--- app/test_api.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = "http://localhost:8000/predict"
-files = {"file": open("data/images/test/000883_JPG_jpg.rf.bcfbe7999ec1b4e660a155e0e145e234.jpg", "rb")}  # Replace with your image
-response = requests.post(url, files=files)
-print(response.json())
